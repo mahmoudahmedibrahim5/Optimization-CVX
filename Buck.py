c99ff1ee84f8494399565c81fbbc0c3a009e5bce
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2024-05-21 22:01:14 +0300

    Cleaner Code

--- Buck.py
+++ Buck.py
@@ -1,29 +1,31 @@
 import cvxpy as cp
 import math
 
+# Optimization variables
 L = cp.Variable(pos = True)
 C = cp.Variable(pos = True)
 Fs = cp.Variable(pos = True)
 
+# Electrical Characteristic
 Vi = 10     # 10 V
 Io = 10     # 10 A
 deltaI = 1.5
 
 # MOSFET Variables
-D = 0
+D = 1
 RDS = 5.2e-3    # 5 milli ohm
 Qrr = 50e-9
 TswON = 10e-8
 TswOFF = 10e-8
 Vf = 0.9
 
+# Impedance
 RL = 2 * math.pi * Fs * L
 RC = 1 / (2 * math.pi * Fs * C)
 
+# Power Losses
 PON = (Io**2 + (deltaI**2) / 12) * D * RDS
 PSW = Vi * (Io - deltaI / 2) * TswON * Fs + Vi * (Io + deltaI / 2) * TswOFF * Fs
-
-# Power Losses
 PQ1 = PON + PSW
 Pd = Vf * Io * (1 - D) + Qrr * Vi * Fs
 Pind = (Io**2 + (deltaI**2) / 12) * RL
@@ -32,18 +34,25 @@ Pcond = ((deltaI**2) / 12) * RC
 # Total Power Loss
 Pbuck = PQ1 + Pd + Pind + Pcond
 
-
-objective = cp.Minimize(Pbuck)
-
-constraints = [
+if __name__ == "__main__":
     
-]
+    objective = cp.Minimize(Pbuck)
 
-problem = cp.Problem(objective, constraints)
+    constraints = [
+        L >= 0.1e-6,
+        L <= 10e-3,
+        C >= 0.1e-6,
+        C <= 100e-6,
+        Fs >= 10000,
+        Fs <= 800000
+    ]
 
-result = problem.solve(gp = True)
+    problem = cp.Problem(objective, constraints)
 
-print("Optimal power loss: ", result)
-print("Optimal Inductance: ", L)
-print("Optimal Capacitance: ", C)
-print("Optimal Frequency: ", Fs)
+    result = problem.solve(gp = True)
+
+    print("Optimal power loss: ", result)
+    print("Optimal Inductance: ", L.value)
+    print("Optimal Capacitance: ", C.value)
+    print("Optimal Frequency: ", Fs.value)
+    
